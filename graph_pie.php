498110306509d11aafa5d6ed750dc48b3a9e8ba5
DamienAlessandrin <72220727+DamienAlessandrin@users.noreply.github.com>
2024-07-03 09:52:33 +0200

    feat: Modification graphique du graph_pie

--- graph_pie.php
+++ graph_pie.php
@@ -3,13 +3,21 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Energy House</title>
+    <link rel="stylesheet" href="styles/styles.css" />
 </head>
 
 <body>
 
-
+    <main>
+        <h1 class="title">Diagramme camembert</h1>
+        <div class="button_list">
+            <a class="button">bouton</a>
+            <a class="button">bouton</a>
+            <a class="button">bouton</a>
+        </div>
+        <div class="graph_pie"></div>
+    </main>
 
 </body>
 
